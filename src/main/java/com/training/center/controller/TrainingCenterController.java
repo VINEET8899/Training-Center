2e239b8482bda9321e7ced34513c36211aff189e
Vineet123-git <shvineet123@gmail.com>
2025-02-15 14:44:48 +0530

    updated code

--- src/main/java/com/training/center/controller/TrainingCenterController.java
+++ src/main/java/com/training/center/controller/TrainingCenterController.java
@@ -3,6 +3,8 @@ package com.training.center.controller;
 import java.io.IOException;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,9 +23,20 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/api/")
 public class TrainingCenterController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(TrainingCenterController.class);
 
 	@Autowired
 	private TrainingCenterService service;
+	
+	/**
+	 * This API is used to create training center
+	 * 
+	 * @param response
+	 * @param createRequest
+	 * @return
+	 * @throws IOException
+	 */
 
 	@PostMapping("training/center/create")
 	public TrainingCenter createTrainingCenter(HttpServletResponse response,
@@ -31,19 +44,27 @@ public class TrainingCenterController {
 		try {
 			return service.createTrainingCenter(createRequest);
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			response.sendError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+			logger.info(e.getMessage());
+			response.sendError(HttpStatus.BAD_REQUEST.value());
 		}
 		return null;
 	}
+	
+	/**
+	 * This API is used to get the list of training centers
+	 * 
+	 * @param response
+	 * @return
+	 * @throws IOException
+	 */
 	
 	@GetMapping("get/training/center/list")
 	public List<TrainingCenter> trainingCenterList(HttpServletResponse response) throws IOException {
 		try {
 			return service.trainingCenterList();
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			response.sendError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+			logger.info(e.getMessage());
+			response.sendError(HttpStatus.BAD_REQUEST.value());
 		}
 		return null;
 	}
